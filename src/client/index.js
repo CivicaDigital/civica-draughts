3e417eb18ed4c47d888261f1f83424ea551a5077
Tom Brady <tom.brady@civica.co.uk>
2018-10-17 13:03:45 +0000

    Added comment for a winning state of FEN

--- src/client/index.js
+++ src/client/index.js
@@ -11,5 +11,6 @@ import queryString from 'query-string';
 import { App } from './App';
 
 const parsed = queryString.parse(window.location.search);
+/* const fenWin = 'W:W15:B1,2,3,4,5,10,12';  A FEN state for a near win */
 
 ReactDOM.render(<App size={8} fen={parsed.fen} />, document.getElementById('root'));
