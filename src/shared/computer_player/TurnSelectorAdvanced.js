7d8348c0af8ef9661dee8ce3058c454b58f73e90
Tom Brady <tom.brady@civica.co.uk>
2018-10-18 08:30:58 +0000

    Fixed the errors in TurnSelectorAdvanced

--- src/shared/computer_player/TurnSelectorAdvanced.js
+++ src/shared/computer_player/TurnSelectorAdvanced.js
@@ -1,30 +1,28 @@
 class TurnSelectorAdvanced {
   static call(turn) {
-  /*  Math.min(...array); finds min value of an array called array */
-    
-    const worstNextTurn = turn.map(turn => getWorstValue(turn));
+    const worstNextTurn = turn.map(x => TurnSelectorAdvanced.getWorstValue(x));
 
     let bestTurn = worstNextTurn[0];
-      for (let i = 0; i < worstNextTurn.length; i++) {
-        if (bestTurn.value < worstNextTurn[i].value) {
-          bestTurn = worstNextTurn[i];
-        }
-
-    /*
-    turn.value = scores.map Math.min(...scores));
-    
-    const bestTurn = turn.reduce((acc, turn) => {
-      if (acc === undefined) acc = turn;
-      if (acc.value < turn.furtherTurns.value) acc = turn;
-      return acc;
-    }, undefined); */
+    for (let i = 0; i < worstNextTurn.length; i++) {
+      if (bestTurn.value < worstNextTurn[i].value) {
+        bestTurn = worstNextTurn[i];
+      }
+    }
   }
 
- static getWorstValue(turn) {
+  static getWorstValue(turn) {
     const scores = turn.furtherTurn.map(value => value.value);
     const worstValue = Math.min(...scores);
     return worstValue;
   }
 }
-
 export default TurnSelectorAdvanced;
+
+/*
+    turn.value = scores.map Math.min(...scores));
+
+    const bestTurn = turn.reduce((acc, turn) => {
+      if (acc === undefined) acc = turn;
+      if (acc.value < turn.furtherTurns.value) acc = turn;
+      return acc;
+    }, undefined); */
